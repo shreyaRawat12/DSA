93a1f2299a887a5927a9d0b333999e3f896580f7
shreyaRawat12 <barkharawat621@gmail.com>
2026-08-09 20:16:00 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (57.42%) - LeetSync

---
+++ 10-regular-expression-matching/regular-expression-matching.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    bool isMatch(string s, string p) {
+        int n = s.length();
+        int m = p.length();
+
+        vector<vector<bool>> dp(n + 1, vector<bool>(m + 1, false));
+
+        // Empty string matches empty pattern
+        dp[0][0] = true;
+
+        // Patterns like a*, a*b*, a*b*c* can match empty string
+        for (int j = 2; j <= m; j++) {
+            if (p[j - 1] == '*') {
+                dp[0][j] = dp[0][j - 2];
+            }
+        }
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+
+                // Normal character or '.'
+                if (p[j - 1] == '.' || p[j - 1] == s[i - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                }
+
+                // '*'
+                else if (p[j - 1] == '*') {
+
+                    // Case 1: Use '*' zero times
+                    dp[i][j] = dp[i][j - 2];
+
+                    // Case 2: Use '*' one or more times
+                    if (p[j - 2] == '.' || p[j - 2] == s[i - 1]) {
+                        dp[i][j] = dp[i][j] || dp[i - 1][j];
+                    }
+                }
+            }
+        }
+
+        return dp[n][m];
+    }
+};
